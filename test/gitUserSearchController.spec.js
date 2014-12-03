02908e4e4a13624d15a7756728b919850734d4fa
Mishal Islam <mishal.islam01@gmail.com>
2014-12-03 19:53:02 +0000

    connected to github API

--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -10,12 +10,22 @@ describe('GitUserSearchController', function() {
     })
   }));
 
+
   it('should initialise with an empty search result and term', function(){
     expect(scope.searchResult).toBeUndefined();
     expect(scope.searchTerm).toBeUndefined();
   });
 
   describe('when searching for a user', function(){
+  var httpBackend;
+  beforeEach(inject(function($httpBackend) {
+    httpBackend = $httpBackend
+    httpBackend
+      .when("GET", "https://api.github.com/search/users?q=hello")
+      .respond({
+        items: items
+      });
+  }));
     var items = [{"login": "mishal1",
         "avatar_url": "https://avatars0.githubusercontent.com/u/8863744?v=3&s=460",
         "html_url": "https://github.com/mishal1"
@@ -27,6 +37,8 @@ describe('GitUserSearchController', function() {
       it('should display search results', function(){
         scope.searchTerm = 'hello';
         scope.doSearch();
+        scope.$apply();
+        httpBackend.flush();
         expect(scope.searchResult.items).toEqual(items);
       });
   }); 
