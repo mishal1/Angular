b5edfb3863ceb9759c892b7495ae30ce56a9628a
Mishal Islam <mishal.islam01@gmail.com>
2014-12-03 18:54:04 +0000

    results are displayed

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,12 +1,15 @@
 githubUserSearch.controller('GitUserSearchController', function($scope) {
-	$scope.searchResult ={
-		"items": [{
-			"login": "mishal1",
-	        "avatar_url": "https://avatars0.githubusercontent.com/u/8863744?v=3&s=460",
-	        "html_url": "https://github.com/mishal1"
-	      },{"login":"abridger",
-	        "avatar_url": "https://avatars1.githubusercontent.com/u/4664049?v=3&s=460",
-	        "html_url": "https://github.com/abridger"
-			}]
+	$scope.doSearch = function(){
+		$scope.searchResult ={
+			"items": [{
+				"login": "mishal1",
+		        "avatar_url": "https://avatars0.githubusercontent.com/u/8863744?v=3&s=460",
+		        "html_url": "https://github.com/mishal1"
+		      },{"login":"abridger",
+		        "avatar_url": "https://avatars1.githubusercontent.com/u/4664049?v=3&s=460",
+		        "html_url": "https://github.com/abridger"
+				}]
+		};
+		
 	};
 });
