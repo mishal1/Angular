35f03af64740048faa79017d6a5cedd0a7bfcadc
Alan Bridger <alanrbridger@gmail.com>
2014-12-04 19:18:29 +0000

    Lists out repos and commits for each user.

--- public/js/gitRepoSearchController.js
+++ public/js/gitRepoSearchController.js
@@ -8,10 +8,36 @@ githubUserSearch.controller('GitRepoSearchController', function($scope, $resourc
       repo: repo,
       client_id: '854d5e8b53f81ffd8ad6',
       client_secret: '3bbf10d29a017cbcd20b87614217396ff67debe6'
-    },{
-      'get': {method: 'GET', isArray:true}
     });
-    console.log($scope.repoSearchResult)
+  };
+
+  var repoStatsResource = $resource('https://api.github.com/repos/:user/:repo/stats/contributors');
+
+  $scope.doRepoStatsSearch = function(username, repo) {
+    $scope.repoStatsSearchResult = repoStatsResource.query({
+        user: username,
+        repo: repo,
+        client_id: '854d5e8b53f81ffd8ad6',
+        client_secret: '3bbf10d29a017cbcd20b87614217396ff67debe6'
+      },{
+        'query': {method: 'GET', isArray:false}
+      });
+    };
+
+});
+
+githubUserSearch.controller('GitRepoListController', function($scope, $resource) {
+
+  var repoListResource = $resource('https://api.github.com/users/:user/repos');
+
+  $scope.getRepoList = function(username) {
+    $scope.repoList = repoListResource.query({
+      user: username,
+      client_id: '854d5e8b53f81ffd8ad6',
+      client_secret: '3bbf10d29a017cbcd20b87614217396ff67debe6'
+    },{
+      'query': {method: 'GET', isArray:true}
+    });
   };
 
 });
